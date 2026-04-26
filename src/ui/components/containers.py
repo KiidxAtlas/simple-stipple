1385dd57b56fb668116573f1f0c3ca2d9a96bcd2
Atlas <KiidxAtlas@users.noreply.github.com>
2026-04-25 17:00:15 -0700

    clean

---
+++ src/ui/components/containers.py
@@ -0,0 +1,15 @@
+"""Compatibility exports for moved canvas/layer-tree widgets."""
+
+from src.ui.components.canvas.widgets import (
+    CanvasPrecisionBar,
+    CanvasStatusStrip,
+    CollapsibleSection,
+    DxfLayersTree,
+)
+
+__all__ = [
+    "CanvasPrecisionBar",
+    "CanvasStatusStrip",
+    "CollapsibleSection",
+    "DxfLayersTree",
+]
